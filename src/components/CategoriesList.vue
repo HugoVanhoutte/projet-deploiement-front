b01397c051d647079364dd0a5d5ec76fa4808786
HugoVanhoutte <hugo.vanhoutte.pro@gmail.com>
2024-11-07 11:29:39 +0100

    feat(footer): added footer

---
+++ src/components/CategoriesList.vue
@@ -0,0 +1,42 @@
+<script setup>
+const props = defineProps({
+  categoryTitle: {
+    type: String,
+    default: 'categorie',
+  },
+  subCategories: {
+    type: Array,
+    required: false,
+  },
+});
+</script>
+
+<template>
+  <h3><router-link to="/">{{ props.categoryTitle }}</router-link></h3>
+  <ul>
+    <li
+      v-for="subCategory in props.subCategories"
+      :key="subCategory"
+    >
+      <router-link to="/">{{ subCategory }}</router-link>
+    </li>
+  </ul>
+</template>
+
+<style scoped lang="scss">
+@import '@/assets/variables';
+a {
+  text-decoration: none;
+  color: inherit;
+  line-height: 1.2rem;
+}
+a:hover {
+  text-decoration: underline;
+}
+h3 {
+  padding-bottom: .75rem;
+  padding-top: 1.25rem;
+  font-size: 1.2rem;
+  text-decoration: underline;
+}
+</style>
---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,63 @@
+<script setup>
+
+import CategoriesList from '@/components/CategoriesList.vue';
+</script>
+
+<template>
+  <footer>
+    <div>
+      <h1>La Meublerie</h1>
+      <categories-list
+        v-for="n in 10"
+        :key="n"
+        categoryTitle="test"
+        :subCategories="['tes1', 'tes2', 'tes3']"
+      />
+    </div>
+    <div>
+      <h1>Informations</h1>
+      <ul>
+        <li><router-link to="/">Conditions d'utilisations</router-link></li>
+        <li><router-link to="/">Politique de confidentialité</router-link></li>
+        <li><router-link to="/">Informations légales</router-link></li>
+      </ul>
+    </div>
+  </footer>
+</template>
+
+<style scoped lang="scss">
+@import '@/assets/variables';
+footer {
+  width: 100%;
+  background-color: $darkmaroon;
+  color: $cornsilk;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  font-family: $body-font;
+  div {
+    display: flex;
+    flex-direction: column;
+    h1 {
+      font-size: 1.5rem;
+      font-weight: 500;
+      padding: 1rem 0;
+    }
+    ul {
+      li {
+        padding: .25rem;
+        a {
+          color: inherit;
+          text-decoration: none;
+          i {
+            font-size: 2rem;
+          }
+        }
+        a:hover {
+          text-decoration: underline;
+        }
+      }
+    }
+  }
+}
+</style>
